59021428d21b1c06d669fac2ad7ea6c4a897e725
addala harshad <64641316+addalaharshad@users.noreply.github.com>
2020-06-30 17:16:47 +0530

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,16 @@
+window.addEventListener("load",()=>{
+    const sounds= document.querySelectorAll(".sound");
+    const pads =  document.querySelectorAll(".pads div");
+    const visual = document.querySelectorAll(".visual");
+    const colors=[
+        "#60d394",
+        "#d36060",
+        "#c060d3"
+       ];
+    pads.forEach((pad,index) =>{
+    	pad.addEventListener("click",function() {
+    		sounds[index].currentTime=0; 
+    		sounds[index].play();
+    	});
+    });
+});
